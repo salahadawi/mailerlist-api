54bd6c8d87abce7f02ff39ede5906cf6954958d3
Salah Adawi <salah.adawi@outlook.com>
2023-03-17 18:17:24 +0200

    fix port being hardcoded

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/subscribe", (req, res) => {
     });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
